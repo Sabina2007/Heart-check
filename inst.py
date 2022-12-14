a3cd9f880b6f2e16b6c142ff946044dfbe4434c7
Sabina2007 <120035432+Sabina2007@users.noreply.github.com>
2022-12-14 19:18:20 +0600

    Create inst.py

---
+++ inst.py
@@ -0,0 +1,6 @@
+from instr import *
+class MainWin(QWidget):
+    def initUI(QWidget):
+        self.hello_text = QLable(text_hello)
+        self.instruction = QLable(text_instruction)
+        self.button = QPushButton(text_nextp)
